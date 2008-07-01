fea6fc11924d59afd103ed954e27f3980df468a4
Thomas Lotze <164362+tlotze@users.noreply.github.com>
2008-07-01 07:31:05 +0000

    started the implementation of get_messages

--- gocept/imapapi/account.py
+++ gocept/imapapi/account.py
@@ -3,6 +3,9 @@
 # $Id$
 
 
+import imaplib
+import email.Parser
+
 import zope.interface
 
 import gocept.imapapi.interfaces
@@ -18,5 +21,17 @@ class Account(object):
         self.user = user
         self.password = password
 
+        self.server = imaplib.IMAP4(host, port)
+        self.server.login(user, password)
+
     def get_messages(self):
-        return []
+        code, data = self.server.select('INBOX')
+        count = int(data[0])
+        msgs = []
+        parser = email.Parser.Parser()
+        for i in xrange(1, count+1):
+            code, data = self.server.fetch(str(i), '(RFC822)')
+            msg_data = data[0]
+            msg_str = msg_data[1]
+            msgs.append(parser.parsestr(msg_str, True))
+        return msgs
