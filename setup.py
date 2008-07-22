d0949866c135b1acd8420b67f557e93e41df3a94
Christian Theune <ct@gocept.com>
2008-07-22 14:45:54 +0000

    Better package metadata and documentation

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # See also LICENSE.txt
 # $Id$
 
+import os
 from setuptools import setup, find_packages
 
 
@@ -10,13 +11,25 @@ setup(
     version='0.1',
     author='gocept gmbh & co. kg',
     author_email='mail@gocept.com',
-    description='Object-oriented API of an IMAP account.',
+    description='Object-oriented API for accessing IMAP accounts.',
     long_description=open(os.path.join(os.path.dirname(__file__), 'gocept',
                                        'imapapi', 'README.txt')).read(),
     packages=find_packages(),
     include_package_data=True,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email :: Post-Office :: IMAP',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
     zip_safe=False,
     license='ZPL 2.1',
     namespace_packages=['gocept'],
-    install_requires=['setuptools'],
+    install_requires=['setuptools',
+                      'zope.interface',
+                      'zope.schema'],
 )
