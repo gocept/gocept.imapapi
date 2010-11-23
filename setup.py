9d480be449d6a7fa735ad673570d520d4f0602ba
Wolfgang Schnerring <ws@gocept.com>
2010-11-23 07:55:09 +0000

    Add changelog to long_description

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2008 gocept gmbh & co. kg
 # See also LICENSE.txt
-# $Id$
 
 import os
 from setuptools import setup, find_packages
@@ -12,8 +11,10 @@ setup(
     author='gocept gmbh & co. kg',
     author_email='mail@gocept.com',
     description='Object-oriented API for accessing IMAP accounts.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'gocept',
-                                       'imapapi', 'README.txt')).read(),
+    long_description=(
+        open(os.path.join(os.path.dirname(__file__),
+                          'gocept', 'imapapi', 'README.txt')).read()
+        + '\n\n' + open('CHANGES.txt').read()),
     packages=find_packages(),
     include_package_data=True,
     classifiers=[
